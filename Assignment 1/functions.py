aaa2ee2c93c3a6e916adbefad775c051f93aa12f
Kilian <kilian.swannet@gmail.com>
2020-04-20 12:26:46 +0200

    working airfoil function, placement of vortex and collocation points, calculation of vorticity values and normal/tangent vectors. solve function faulty

--- Assignment 1/functions.py
+++ Assignment 1/functions.py
@@ -1,18 +1,33 @@
 import numpy as np
-from math import pi, sin, cos, atan
+from math import pi, sin, cos, atan, sqrt
 
+def airfoil(Naca=[2, 4, 1, 0], n_panels=10):
+    """
+    From a given 4-digit NACA code, finds the function of the camber line,
+    thickness distribution, and upper and lower surface points
 
-def airfoil(Naca=[2, 4, 1, 0], N=101):
+    Args:
+        Naca (list, optional): list containing 4 digits of a NACA airfoil.
+                               Defaults to [2, 4, 1, 0].
+        n_panels (int, optional): Number of panels.
+                           Defaults to 10.
 
+    Returns:
+        coor_u (list): List of coordinates of the upper airfoil surface.
+        coor_l (list): List of coordinates of the lower airfoil surface.
+        coor_c (list): List of coordinates of points on the camber line.
+    """
     M = Naca[0]/100
     P = Naca[1]/10
     t_c = float("0.{}{}".format(Naca[2], Naca[3]))
+
+    N = n_panels + 1        # Number of panel edges
 
     a0 = 0.2969
     a1 = -0.126
     a2 = -0.3516
     a3 = 0.2843
-    a4 = -0.1015        # -0.1036 for closed TE, -0.1015 for open TE
+    a4 = -0.1015            # -0.1036 for closed TE, -0.1015 for open TE
 
     coor_u = []
     coor_l = []
@@ -20,7 +35,7 @@ def airfoil(Naca=[2, 4, 1, 0], N=101):
     coor_t = []
 
     # using NACA 4 digit airfoil calculation to
-    # obtain airfoil coordinate points
+    # obtain airfoil coordinate points = panel edges
     for beta in np.linspace(0, pi, N):
         # cosine spacing
         x = 0.5 - 0.5 * cos(beta)
@@ -45,7 +60,8 @@ def airfoil(Naca=[2, 4, 1, 0], N=101):
 
         theta = atan(dyc_dx)
 
-        # exact position perpendicular to camber line
+        # exact position of upper and lower surface coordinates
+        # perpendicular to camber line
         x_u = x - y_t * sin(theta)
         y_u = y_c + y_t * cos(theta)
         x_l = x + y_t * sin(theta)
@@ -53,20 +69,34 @@ def airfoil(Naca=[2, 4, 1, 0], N=101):
 
         coor_u.append([x_u, y_u])   # upper airfoil coordinates
         coor_l.append([x_l, y_l])   # lower airfoil coordinates
-        coor_c.append([x, y_c])     # camber line coordinates
         coor_t.append([x, y_t])     # thickness distribution
+        coor_c.append([x, y_c])     # camber line coordinates = panel edges
 
     return coor_u, coor_l, coor_c
 
 
-def panel_points(coor):
+def panels(coor_c):
+    """
+    takes a list of points on the camber line. These points are considered
+    panel edges. Collocation points are added at 0.75 panel length and vortex 
+    points are added at 0.25 panel lenght.
 
+    Args:
+        coor_c (list): List of panel edges (on camber line).
+                       should be list of camberline points.
+
+    Returns:
+        coor_col (list): list of coordinates of collocation points.
+        coor_vor (list): list of coordinates of vortex element points.
+    """
     coor_col = []
     coor_vor = []
+    thetas = []
+    panel_length = []
 
-    for i in range(len(coor)-1):
-        p1 = coor[i]
-        p2 = coor[i+1]
+    for i in range(len(coor_c)-1):
+        p1 = coor_c[i]
+        p2 = coor_c[i+1]
 
         # place collocation point at 0.75 panel length
         x_col = p1[0] + 0.75 * (p2[0] - p1[0])  # x_i
@@ -78,4 +108,58 @@ def panel_points(coor):
         y_vor = p1[1] + 0.25 * (p2[1] - p1[1])  # x_j
         coor_vor.append([x_vor, y_vor])
 
-    return coor_col, coor_vor
+        # find panel angle
+        theta = atan((p1[1] - p2[1]) / (p2[0] - p1[0]))
+        thetas.append(theta)
+
+        # panel length
+        l = sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
+        panel_length.append(l)
+
+    return coor_col, coor_vor, thetas, panel_length
+
+
+def v_ind(x_col, y_col, x_vor, y_vor, Gamma=1):
+    """
+    Finds induced velocity at point x_col, y_col due to a vortex
+    with circulation Gamma at x_vor, y_vor
+
+    Args:
+        x_col (Float): x location of evaluation point
+        y_col (Float): y location of evaluation point
+        x_vor (Float): x location of vortex element
+        y_vor (Float): y location of vortex element
+        Gamma (Float): Vorticity. Defaults to unit strength 1.
+
+    Returns:
+        u (Float): horizontal component of induced velocity due to vortex
+        y (Float): vertical component of induced velocity due to vortex
+    """
+
+    r = sqrt( (x_col - x_vor)**2 + (y_col - y_vor)**2 )
+    u = Gamma/(2*pi) * (y_col - y_vor)/r**2         # horizontal component
+    w = -Gamma/(2*pi) * (x_col - x_vor)/r**2        # vertical component
+    return np.array([u, w])
+
+
+def normals(thetas):
+    """
+    Return the normal and tangent vectors of each panel from panel angels
+
+    Args:
+        thetas (list): List of panel angels
+
+    Returns:
+        normals (list): list of numpy arrays containing the normal vectors for
+                        all panels
+        tangents (list): list of numpy arrays containing the tangent vectors
+                         for all panels
+    """
+    normals = []
+    tangents = []
+    for theta in thetas:
+        n_i = np.array([sin(theta), cos(theta)])    # normal vector
+        t_i = np.array([cos(theta), -sin(theta)])   # tangent vector
+        normals.append(n_i)
+        tangents.append(t_i)
+    return normals, tangents
--- Assignment 1/panelMethod.py
+++ Assignment 1/panelMethod.py
@@ -1,45 +1,100 @@
 import numpy as np
-from math import pi, sin, cos, atan, atan2
-from functions import airfoil, panel_points
+from math import pi, sin, cos, atan, atan2, sqrt
+from functions import airfoil, panels, v_ind, normals
 import matplotlib.pyplot as plt
 
 # pg280 573 300
 
 # * Constants * #
-alpha = 10 * pi/180
+alpha = 0 * pi/180
 v_inf = 1.
 u_inf = v_inf * cos(alpha)
 w_inf = v_inf * sin(alpha)
+Q_inf = np.array([[u_inf, w_inf]])
 rho = 1.225
-q = 0.5 * rho * v_inf**2
+p_0 = 101325
+# q = 0.5 * rho * v_inf**2
 
-n_panels = 5           # Number of panels
-N = n_panels + 1        # Number of panel edges
+n_panels = 50            # Number of panels
+NACA = [2, 4, 0, 6]     # NACA XXXX digits of 4-digit airfoil
 
-NACA = [9, 4, 2, 0]     # NACA XXXX digits of 4-digit airfoil
-t_c = 0.14              # airfoil thickness
-
-# * obtaining panel edge, collocation and vortex points * #
-coor_u, coor_l, coor_c = airfoil(NACA, N)
-coor_col, coor_vor = panel_points(coor_c)
+coor_u, coor_l, coor_c = airfoil(NACA, n_panels)
 
 
+def solve_vorticity(surface):
+    coor_col, coor_vor, panel_angle, panel_length = panels(surface)
+    normal, tangent = normals(panel_angle)
+
+    A = np.zeros((n_panels, n_panels))
+    RHS = np.zeros((n_panels, 1))
+
+    for i, col in enumerate(coor_col):
+        for j, vor in enumerate(coor_vor):
+            q_ij = v_ind(col[0], col[1], vor[0], vor[1], 1)
+            a_ij = np.dot(q_ij, normal[i])
+            A[i, j] = a_ij
+        RHS[i] = np.dot(-Q_inf, np.transpose(normal[i]))
+
+    Gamma = np.linalg.solve(A, RHS)
+
+    return Gamma, panel_length, coor_col
 
 
+def solve_cp(Gamma, panel_length):
+    dCp = dL = dP = np.zeros((n_panels, 1))
 
-# Get all x- or y-values from a coordinate set for easier plotting
-# i = 0 for x values, i = 1 for y values
+    for i, gamma in enumerate(Gamma):
+        dL[i] = rho * v_inf * gamma
+        dP[i] = rho * v_inf * gamma / panel_length[i]
+
+        dCp[i] = (dP[i] - 101325)/(0.5*1.225*v_inf**2)
+
+    return dL, dP, dCp
+
+Gamma_u, panel_length_u, col_u = solve_vorticity(coor_u)
+Gamma_l, panel_length_l, col_l = solve_vorticity(coor_l)
+
+_, dP_u, dCp_u = solve_cp(Gamma_u, panel_length_u)
+_, dP_l, dCp_l = solve_cp(Gamma_l, panel_length_l)
+
+# * plotting * #
+
 def pt(set, i):
+    """
+    Get all x- or y-values from a coordinate set for easier plotting.
+    i = 0 for x values, i = 1 for y values
+
+    Args:
+        set (list): list of coordinates
+        i (integer): Set i=0 for a list of x values, set i=1 for a list of all
+        y vlaues
+
+    Returns:
+        [list]: list of all x- or all y-values from a list of coordinates
+    """
     p = []
     for point in set:
         p.append(point[i])
     return p
 
-plt.plot(
-        #  pt(coor_u, 0), pt(coor_u, 1), 'b',
-        #  pt(coor_l, 0), pt(coor_l, 1), 'b',
-         pt(coor_c, 0), pt(coor_c, 1), 'k',
-         pt(coor_vor, 0), pt(coor_vor, 1), 'ob',
-         pt(coor_col, 0), pt(coor_col, 1), 'xr')
-plt.axis('equal')
+# plt.figure(1)
+# plt.plot(pt(coor_c, 0), pt(coor_c, 1), 'k',
+#          pt(coor_vor, 0), pt(coor_vor, 1), 'ob',
+#          pt(coor_col, 0), pt(coor_col, 1), 'xr',
+#          pt(coor_u, 0), pt(coor_u, 1), 'b',
+#          pt(coor_l, 0), pt(coor_l, 1), 'b',
+#          )
+# plt.axis('equal')
+# plt.show()
+
+plt.figure(2)
+plt.plot(pt(col_u, 0), -dCp_u)
+plt.plot(pt(col_l, 0), -dCp_l)
+# plt.axis('equal')
 plt.show()
+
+# plt.figure(2)
+# plt.plot(pt(coor_c, 0), -dCpu)
+# plt.plot(pt(coor_c, 0), -dCpl)
+# # plt.axis('equal')
+# plt.show()
