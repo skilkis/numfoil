d33d5df9340664ae5599e9d41564345b5182efef
Kilian <kilian.swannet@gmail.com>
2020-05-02 18:22:10 +0200

    Add sample test of vorticity calculations

---
+++ tests/test_gammapy/test_vorticity.py
@@ -0,0 +1,34 @@
+# Copyright 2020 Kilian Swannet, San Kilkis
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+from gammapy.functions import airfoil, v_comp
+from gammapy.panel.vortex_sheet import solve_vorticity as solve_vorticity
+import numpy as np
+from math import sin, pi
+
+
+# Katz&Plotkin pg267
+def test_vorticity():
+    """..."""
+    Q_inf = v_comp(1, 1)
+    _, _, coor_c = airfoil([0, 0, 0, 0], 5)
+    Gamma, panel_length, _, _ = solve_vorticity(coor_c, Q_inf)
+    ref = np.array([[2.46092],
+                    [1.09374],
+                    [0.70314],
+                    [0.46876],
+                    [0.27344]])
+    calc = Gamma / (pi * np.array([panel_length]).T * sin(1))
+    
+    pass
