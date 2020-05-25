e300eceb78ff57ff4b283c122c84a4753f05cbc0
skilkis <sankilkis@msn.com>
2020-05-26 01:26:41 +0200

    Add linear strength panel method implementation
    This commit rewrites the code now contained in the legacy/ directory to
    be a PanelMethod class implementation. Furthermore, it fully vectorizes
    the panel method using Numpy and Numba resulting in more than a 10x
    increase in performance.
    
    Summary of Changes:
    - Added linear vortex strength panel method implementation
    - Added unit tests for the new PanelMethod implementation using
      verification data
    - Added verification data as a Numpy binary file
    - Replace unused .exe LFS entry to .npy to store binary verification
      data using git LFS

---
+++ src/gammapy/solver/m_linear_vortex.py
@@ -0,0 +1,251 @@
+# Copyright 2020 Kilian Swannet, San Kilkis
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+"""Contains definitions for a Linear Strength Vortex Panel Method."""
+
+import math
+from functools import cached_property
+from typing import Dict, Sequence, Tuple, Union
+
+import numba
+import numpy as np
+
+from gammapy.geometry.panel import Panel2D
+from gammapy.solver.base import BASE_NUMBA_CONFIG, FlowSolution, PanelMethod
+
+
+class LinearVortex(PanelMethod):
+    """Implements a Linear Strength Vortex panel method."""
+
+    @cached_property
+    def unit_rhs_vector(self) -> np.ndarray:
+        """Normalized right-hand-side consisting of panel normals."""
+        # Final entry is (0, 0) which will enforce the Kutta condition
+        normals = np.zeros((self.panels.n_panels + 1, 2), dtype=np.float64)
+        normals[:-1, :] = self.panels.normals
+        return normals
+
+    @cached_property
+    def panels(self) -> Panel2D:
+        """Panels that run from TE -> Bottom -> Top -> TE."""
+        sample_u = PanelMethod.get_sample_parameters(
+            num=(self.n_panels // 2) + 1, spacing=self.spacing
+        )
+        bot_pts = self.airfoil.lower_surface_at(sample_u[::-1])
+        top_pts = self.airfoil.upper_surface_at(sample_u[1:])
+        # return Panel2D(np.vstack((bot_pts, top_pts, ((1.25, 0)))))
+        return Panel2D(np.vstack((bot_pts, top_pts)))
+
+    @cached_property
+    def collocation_points(self) -> np.ndarray:
+        """Collocation points located at the midpoint of each panel."""
+        return self.panels.points_at(0.5)
+
+    @cached_property
+    def influence_matrices(self) -> Dict[str, np.ndarray]:
+        """Normal and tangent influence coefficient matrices."""
+        start_pts, _ = self.panels.nodes
+        im_normal, im_tangent = calc_linear_vortex_im(
+            col_pts=self.collocation_points,
+            vort_pts=start_pts,  # Vortices are placed on all start nodes
+            panel_angles=self.panels.angles,
+            panel_lengths=self.panels.lengths,
+        )
+        return {"normal": im_normal, "tangent": im_tangent}
+
+    @property
+    def influence_matrix(self) -> np.ndarray:
+        """Normal influence matrix for :py:meth`get_circulations`."""
+        return self.influence_matrices["normal"]
+
+    def solve_for(
+        self, alpha: Union[float, Sequence[float]], plot: bool = False,
+    ) -> FlowSolution:
+        """Returns a :py:class:`FlowSolution` with lazy attributes.
+
+        Args:
+            alpha: A value or sequence of Angle of Attack in SI degree
+            plot: Sets if plots should be shown on initialization.
+                This evaluates all attributes and negates the
+                laziness of the returned object. Defaults to 1.
+        """
+        return self.solution_class(
+            method=self,
+            # Removing constant error term in the solution
+            circulations=self.get_circulations(alpha)[:-1, :],
+            alpha=alpha,
+        )
+
+
+@numba.jit(**BASE_NUMBA_CONFIG)
+def calc_integration_constants(  # noqa: D103
+    col_pt: numba.float64[:],
+    vort_pt: numba.float64[:],
+    col_panel_angle: float,
+    vort_panel_angle: float,
+    vort_panel_length: float,
+) -> Tuple[float, float, float, float, float, float, float, float, float]:
+    """Calculates integration constants of the linear vortex method.
+
+    Refer to pg. 157-158 of Foundations of Aerodynamics: 5th Edition or
+    watch JoshTheEngineer's `video_`.
+
+    Args:
+        col_pt: Collocation point to measure induced velocity
+        vort_pt: Start node (point) of a linear vortex panel
+        col_panel_angle: Panel angle of the collocation point panel
+        vort_panel_angle: Panel angle of the linear vortex panel
+        vort_panel_length: Length of the linear vortex panel
+
+    Returns:
+        A, B, C, D, E, F, G, P, Q integration constants of the
+        linear vortex panel method for airfoils of arbitrary thickness.
+
+    .. _video: https://www.youtube.com/watch?v=5lmIv2CUpoc
+    """
+
+    v = col_pt - vort_pt  # Vector from vortex to collocation point
+    vx, vy = v  # Vector from vortex to collocation point
+    s_j = vort_panel_length  # Length of the vortex panel
+
+    a = -vx * math.cos(vort_panel_angle) - vy * math.sin(vort_panel_angle)
+    b = np.sum(v ** 2)
+    c = math.sin(col_panel_angle - vort_panel_angle)
+    d = math.cos(col_panel_angle - vort_panel_angle)
+    e = vx * math.sin(vort_panel_angle) - vy * math.cos(vort_panel_angle)
+    f = math.log(1 + ((s_j ** 2 + 2 * s_j * a) / b))
+    g = math.atan2(e * s_j, b + a * s_j)
+
+    # Turning Black formatter off here for redability
+    # fmt: off
+    p = vx * math.sin(col_panel_angle - 2 * vort_panel_angle) \
+        + vy * math.cos(col_panel_angle - 2 * vort_panel_angle)
+    q = vx * math.cos(col_panel_angle - 2 * vort_panel_angle) \
+        - vy * math.sin(col_panel_angle - 2 * vort_panel_angle)
+    # fmt: on
+
+    return a, b, c, d, e, f, g, p, q
+
+
+@numba.jit(**BASE_NUMBA_CONFIG)
+def calc_vortex_coefficients(  # noqa: D103
+    col_pt: numba.float64[:],
+    vort_pt: numba.float64[:],
+    col_panel_angle: float,
+    vort_panel_angle: float,
+    vort_panel_length: float,
+) -> Tuple[float, float]:
+    """Calculates induced velocity coefficients.
+
+    Refer to pg. 157-159 of Foundations of Aerodynamics: 5th Edition.
+
+    Args:
+        col_pt: Collocation point to measure induced velocity
+        vort_pt: Start node (point) of a linear vortex panel
+        col_panel_angle: Panel angle of the collocation point panel
+        vort_panel_angle: Panel angle of the linear vortex panel
+        vort_panel_length: Length of the linear vortex panel
+
+    Returns:
+        CN_1, CN_2, CT_1, CT_2 induced velocity coefficients. The
+        letter N and T refer to the normal and tangential induced
+        velocities respectively. Furthermore, the subscript 1 is the
+        velocity starting intensity
+    """
+    a, b, c, d, e, f, g, p, q = calc_integration_constants(
+        col_pt, vort_pt, col_panel_angle, vort_panel_angle, vort_panel_length,
+    )
+    s_j = vort_panel_length
+
+    cn_2 = d + (0.5 * q * f / s_j) - (a * c + d * e) * g / s_j
+    cn_1 = (0.5 * d * f) + (c * g) - cn_2
+
+    ct_2 = c + (0.5 * p * f / s_j) + (a * d - c * e) * g / s_j
+    ct_1 = (0.5 * c * f) - (d * g) - ct_2
+
+    return cn_1, cn_2, ct_1, ct_2
+
+
+@numba.jit(parallel=True, **BASE_NUMBA_CONFIG)
+def calc_linear_vortex_im(  # noqa: D103
+    col_pts: numba.float64[:, :],
+    vort_pts: numba.float64[:, :],
+    panel_angles: numba.float64[:, :],
+    panel_lengths: numba.float64[:, :],
+) -> numba.float64[:, :, :]:
+    """Calculates the normal and tangent influence coefficient matrices.
+
+    This is meant to be used with the :py:class:`LinearVortex` panel
+    method which assumes that there are n+1 vortices of unknown
+    circulation per unit length gamma. Therefore, the Kutta-condition
+    can be satisfied without deleting or adding any additional
+    equations.
+
+    Args:
+        start_pts: Start nodes (points) of all panels
+        end_pts: End nodes (points) of all panels
+        col_pts: Collocation points placed at the midpoint of each panel
+        panel_angles: Panel angles in SI radian as a column vector
+        panel_normals: Panel normal vectors as a set of row vectors
+        panel_tangents: Panel tangent vectors as a set of row vectors
+
+    Returns:
+        Normal and tangent vortex influence matrices for the Linear
+        Strength Vortex method returned as a 3D numpy array. The shape
+        of the returned array is (2, n_panels + 1, n_panels +1) thus the
+        normal and tangent vortex influence matrics can be unpacked as
+        follows::
+
+            im_normal, im_tangent = calc_linear_vortex_im(*args)
+    """
+    n_vorts, _ = vort_pts.shape
+    n_cols, _ = col_pts.shape
+
+    influence_matrix = np.zeros((2, n_cols + 1, n_vorts + 1), dtype=np.float64)
+
+    # cn = Normal induced velocity coefficient
+    # ct = Tangent induced velocity coefficient
+    # Subscript 1 = Panel start value
+    # Subscript 2 = Panel end value
+    for i in numba.prange(n_cols):
+
+        col_pt = col_pts[i]
+        col_panel_angle = panel_angles[i, 0]
+
+        # Initial end value coefficients are zero
+        cn_2_old, ct_2_old = float(0), float(0)
+        for j in range(n_vorts):
+            if i == j:
+                cn_1, cn_2, ct_1, ct_2 = -1, 1, math.pi / 2, math.pi / 2
+            else:
+                cn_1, cn_2, ct_1, ct_2 = calc_vortex_coefficients(
+                    col_pt=col_pt,
+                    vort_pt=vort_pts[j],
+                    col_panel_angle=col_panel_angle,
+                    vort_panel_angle=panel_angles[j, 0],
+                    vort_panel_length=panel_lengths[j, 0],
+                )
+            # Storing normal/tangent coefficient and updating end value
+            influence_matrix[..., i, j] = cn_1 + cn_2_old, ct_1 + ct_2_old
+            cn_2_old, ct_2_old = cn_2, ct_2
+
+        # Vortex at TE gets final end value coefficients
+        influence_matrix[..., i, j + 1] = cn_2_old, ct_2_old
+
+    # Inserting kutta condition gamma_0 + gamma_n+1 = 0 for normal
+    # coefficient matrix (Index = 0)
+    influence_matrix[0, -1, 0] = 1
+    influence_matrix[0, -1, -1] = 1
+
+    return influence_matrix
---
+++ tests/test_gammapy/test_solver/test_m_linear_vortex.py
@@ -0,0 +1,142 @@
+# Copyright 2020 Kilian Swannet, San Kilkis
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+from gammapy.geometry.airfoil import NACA4Airfoil
+from gammapy.geometry.panel import Panel2D
+from gammapy.solver.base import PanelMethod
+from gammapy.solver.m_linear_vortex import (
+    calc_integration_constants,
+    calc_linear_vortex_im,
+    calc_vortex_coefficients,
+)
+
+REL_TOL = 5e-5
+DATA_DIR = Path(__file__).parent / "data"
+
+
+@pytest.fixture(scope="module")
+def naca2412():
+    """Returns a NACA2412 :py:class:`NACA4Airfoil` object."""
+    return NACA4Airfoil(naca_code="naca2412", te_closed=True)
+
+
+@pytest.fixture(scope="module")
+def panels(naca2412):
+    """Returns a paneled version of a NACA2412 airfoil."""
+    sample_u = PanelMethod.get_sample_parameters(num=6, spacing="cosine")
+    bot_pts = naca2412.lower_surface_at(sample_u[::-1])
+    top_pts = naca2412.upper_surface_at(sample_u[1:])
+    return Panel2D(np.vstack((bot_pts, top_pts)))
+
+
+def test_calc_integration_constants(panels):
+    """Tests that integration constants are calculated correctly.
+
+    The test case calculates the coefficients due to the influence of
+    vortex (n - 1) on panel (n)
+    """
+    expected_result = dict(
+        a=-0.30074680202253584,
+        b=0.0904534136839456,
+        c=-0.045227355204173834,
+        d=0.9989767196192489,
+        e=0.0021851217730942327,
+        f=-3.6571407713042885,
+        g=0.03797726355934574,
+        p=-0.015784868222446846,
+        q=0.30034022644185643,
+    )
+    for i, panel in enumerate(panels):
+        panel
+    second_to_last_panel = panels[-2]
+    last_panel = panels[-1]
+    start_pt, _ = second_to_last_panel.nodes
+    kwargs = dict(
+        col_pt=last_panel.points_at(0.5)[0, :],
+        vort_pt=start_pt[0, :],
+        col_panel_angle=last_panel.angles[0, 0],
+        vort_panel_angle=second_to_last_panel.angles[0, 0],
+        vort_panel_length=second_to_last_panel.lengths[0, 0],
+    )
+
+    jit_result = calc_integration_constants(**kwargs)
+    result = calc_integration_constants.py_func(**kwargs)
+
+    for i, coef in enumerate(expected_result.keys()):
+        assert jit_result[i] == pytest.approx(
+            expected_result[coef], rel=REL_TOL
+        )
+        assert result[i] == pytest.approx(expected_result[coef], rel=REL_TOL)
+
+
+CALC_VORTEX_COEFFICIENTS_TEST_CASES = {
+    # Expected result is of the form (cn_1, cn_2, ct_1, ct_2)
+    "argnames": "col_idx, vort_idx, expected_result",
+    "argvalues": [
+        # Testing influence of vortex gamma_2 on collocation point 1
+        (0, 1, (1.17573979, 0.64921316, 3.42384684e-03, 2.98904500e-03)),
+        # Testing influence of vortex gamma_10 on collocation point 1
+        (0, 9, (0.31826418, -0.95201426, 1.40537373e00, 1.13522801e00)),
+        # Testing influence of vortex gamma_3 on collocation point 8
+        (7, 2, (0.44829642, -0.25604628, 9.64947195e-01, 1.01300764e00)),
+    ],
+}
+
+
+@pytest.mark.parametrize(**CALC_VORTEX_COEFFICIENTS_TEST_CASES)
+def test_calc_vortex_coefficients(panels, col_idx, vort_idx, expected_result):
+    """Tests influence coefficient results for various panels."""
+
+    col_panel = panels[col_idx]
+    vort_panel = panels[vort_idx]
+    kwargs = dict(
+        col_pt=col_panel.points_at(0.5)[0, :],
+        vort_pt=vort_panel.nodes[0][0, :],
+        col_panel_angle=col_panel.angles[0, 0],
+        vort_panel_angle=vort_panel.angles[0, 0],
+        vort_panel_length=vort_panel.lengths[0, 0],
+    )
+
+    jit_result = calc_vortex_coefficients(**kwargs)
+    result = calc_vortex_coefficients.py_func(**kwargs)
+
+    assert jit_result == pytest.approx(expected_result, rel=REL_TOL)
+    assert result == pytest.approx(expected_result, rel=REL_TOL)
+
+
+def test_calc_linear_vortex_im(panels):
+    """Tests influence matrix values against verification data."""
+    kwargs = dict(
+        col_pts=panels.points_at(0.5),
+        vort_pts=panels.nodes[0],  # Start nodes
+        panel_angles=panels.angles,
+        panel_lengths=panels.lengths,
+    )
+
+    jit_result = calc_linear_vortex_im(**kwargs)
+    result = calc_linear_vortex_im.py_func(**kwargs)
+
+    an = np.load(DATA_DIR / "an_matrix_naca2412.npy")
+    at = np.load(DATA_DIR / "at_matrix_naca2412.npy")
+
+    assert jit_result[0, :-1, :-1] == pytest.approx(an[:-1, :-1], rel=REL_TOL)
+    assert jit_result[1, :-1, :-1] == pytest.approx(at[:-1, :-1], rel=REL_TOL)
+
+    assert result[0, :-1, :-1] == pytest.approx(an[:-1, :-1], rel=REL_TOL)
+    assert result[1, :-1, :-1] == pytest.approx(at[:-1, :-1], rel=REL_TOL)
