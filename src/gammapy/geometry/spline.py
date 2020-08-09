7cefe2fc4804c1a89573682419ff98c568b47b26
skilkis <sankilkis@msn.com>
2020-08-09 15:56:42 +0200

    Add ability to create 2D splines from points
    This commit adds the ability to create b-splines using the Scipy library
    from a list of points. This is useful for interpolating airfoil
    curves from an airfoil coordinate file for use in panelling or analysis.
    
    Summary of Changes:
    - Add BSpline2D class to be able to create 2D splines from points
    - Add preliminary unit-tests for the BSpline2D class

---
+++ src/gammapy/geometry/spline.py
@@ -0,0 +1,48 @@
+"""Contains py:class:`BSpline` for splining 2D points."""
+
+from functools import cached_property
+from typing import Optional, Union
+
+import numpy as np
+import scipy.interpolate as si
+
+from .vector2d import normalize_2d, rotate_2d_90ccw
+
+
+class BSpline2D:
+    """Creates a splined representation of a set of points.
+
+    Args:
+        points: A set of 2D row-vectors
+        degree: Degree of the spline. Defaults to 3 (cubic spline).
+    """
+
+    def __init__(self, points: np.ndarray, degree: Optional[int] = 3):
+        self.points = points
+        self.degree = degree
+
+    @cached_property
+    def spline(self):
+        """1D spline representation of :py:attr:`points`.
+
+        Returns:
+            Scipy 1D spline representation:
+                [0]: Tuple of knots, the B-spline coefficients, degree
+                     of the spline.
+                [1]: Parametric points, u, used to create the spline
+        """
+        return si.splprep(self.points.T, s=0.0, k=self.degree)
+
+    def evaluate_at(self, u: Union[float, np.ndarray]) -> np.ndarray:
+        """Evaluate the spline point(s) at ``u``."""
+        return np.array(si.splev(u, self.spline[0], der=0), dtype=np.float64).T
+
+    def tangent_at(self, u: Union[float, np.ndarray]) -> np.ndarray:
+        """Evaluate the spline tangent(s) at ``u``."""
+        return normalize_2d(
+            np.array(si.splev(u, self.spline[0], der=1), dtype=np.float64).T
+        )
+
+    def normal_at(self, u: Union[float, np.ndarray]) -> np.ndarray:
+        """Evaluate the spline normals(s) at ``u``."""
+        return rotate_2d_90ccw(self.tangent_at(u))
---
+++ tests/test_gammapy/test_geometry/test_spline.py
@@ -0,0 +1,63 @@
+# Copyright 2020 Kilian Swannet, San Kilkis
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import numpy as np
+
+from gammapy.geometry.spline import BSpline2D
+from tests.test_gammapy.helpers import ScenarioTestSuite
+
+
+class TestBSpline2D(ScenarioTestSuite):
+
+    SCENARIOS = {
+        "line": BSpline2D(
+            np.array([[0, 0], [1, 0], [2, 0], [3, 0]], dtype=np.float64)
+        ),
+        "reversed_line": BSpline2D(
+            np.array([[3, 0], [2, 0], [1, 0], [0, 0]], dtype=np.float64)
+        ),
+    }
+
+    EXPECTED_EVALUATE_AT = {
+        "line": np.array([[0, 0], [1.5, 0], [3.0, 0]]),
+        "reversed_line": np.array(([3.0, 0], [1.5, 0], [0, 0])),
+    }
+
+    def test_evaluate_at(self, scenario):
+        """Tests if points on the spline are evaluated correctly."""
+        obj, label = scenario
+        result = obj.evaluate_at(np.array([0, 0.5, 1], dtype=np.float64))
+        assert np.allclose(result, self.EXPECTED_EVALUATE_AT[label])
+
+    EXPECTED_TANGENT_AT = {
+        "line": np.array([[1, 0]], dtype=np.float64),
+        "reversed_line": np.array([[-1, 0]], dtype=np.float64),
+    }
+
+    def test_tangent_at(self, scenario):
+        """Tests if the correct tangent vector is returned."""
+        obj, label = scenario
+        result = obj.tangent_at(0.5)
+        assert np.allclose(result, self.EXPECTED_TANGENT_AT[label])
+
+    EXPECTED_NORMAL_AT = {
+        "line": np.array([[0, 1]], dtype=np.float64),
+        "reversed_line": np.array([[0, -1]], dtype=np.float64),
+    }
+
+    def test_normal_at(self, scenario):
+        """Tests if the correct normal vector is returned."""
+        obj, label = scenario
+        result = obj.normal_at(0.5)
+        assert np.allclose(result, self.EXPECTED_NORMAL_AT[label])
